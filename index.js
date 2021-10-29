e6baba4c2da327f84a031c512002cc459a4d3471
nhmunna <munna1720.nhm@gmail.com>
2021-10-30 01:46:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2gdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get('/', (req, res) => {
+    res.send('Tourism Server is running')
+});
+
+app.listen(port, () => {
+    console.log('Server is running at port', port)
+})
